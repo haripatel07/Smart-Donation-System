416618dfb4d8ebc112cbd716f5b0b3d1d9216369
Hari Patel <patelhariv18@gmail.com>
2025-09-19 21:28:35 +0530

    Create app.js

---
+++ Backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+
+// Import Routes
+const authRoutes = require("./routes/auth");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Connect Database
+connectDB();
+
+// Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
