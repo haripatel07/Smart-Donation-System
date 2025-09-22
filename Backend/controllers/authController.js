3d5d40308b96a90e59a5c078182398812ce644e5
Hari Patel <patelhariv18@gmail.com>
2025-09-22 12:59:17 +0530

    Create authController.js

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,18 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ msg: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, password: hashedPassword, role });
+    await newUser.save();
+
+    res.status(201).json({ msg: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
